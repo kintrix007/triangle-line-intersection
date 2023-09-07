b6cc27e1c3d5988fb2b4377cd59d5ea993fad43e
kintrix007 <boldizsar02@gmail.com>
2023-09-07 12:17:09 +0200

    feat: Add more utilities to Vector3

--- src/Tri.hs
+++ src/Tri.hs
@@ -1,11 +1,40 @@
 module Tri
   (Tri (Tri)
-  ) where
+  , solve
+  , isOnTri) where
 
 import           Vector3
+import Debug.Trace (trace)
 
 data Tri = Tri
   { a :: !Vector3
   , b :: !Vector3
   , c :: !Vector3 }
   deriving (Show, Eq)
+
+isOnTri :: Vector3 -> Tri -> Bool
+isOnTri vap (Tri a b c) =
+  case do {
+      (x1, x2) <- solve vab vac vap;
+      if x1 >= 0 && x2 >= 0
+        then solve vba vbc vbp
+        else Nothing;
+    } of 
+    Nothing       -> False
+    Just (x1, x2) -> x1 >= 0 && x2 >= 0
+  where
+    vab = b - a
+    vac = c - a
+    vba = a - b
+    vbc = c - b
+    vbp = vap - vab
+
+solve :: Vector3 -> Vector3 -> Vector3 -> Maybe (Double, Double)
+solve (Vector3 a1 b1 c1) (Vector3 a2 b2 c2) (Vector3 x y z)
+  | thing == 0 && False = Nothing
+  | otherwise = Just (x1, x2)
+  where
+    thing = z - c1/a1*x - ((c2 - c1/a1*a2) / (b2 - b1/a1*a2))*(y - b1/a1*x)
+    x1 = (x - a2/(b2 - b1/a1*a2)*y - b1/a1*x) / a1
+    x2 = (y - b1/a1*x) / (b1 - b1/a1*a2)
+
--- src/Vector3.hs
+++ src/Vector3.hs
@@ -1,14 +1,17 @@
 {-# LANGUAGE InstanceSigs #-}
 module Vector3
-    ( Vector3 (Vector3)
-    , magnitudeSquared
-    , dot
-    , magnitude) where
+  ( Vector3 (Vector3)
+  , magnitudeSquared
+  , dot
+  , toVec
+  , magnitude
+  , normalize
+  , project) where
 
 data Vector3 = Vector3
-  { x :: !Double
-  , y :: !Double
-  , z :: !Double }
+  !Double -- ^ x coord
+  !Double -- ^ y coord
+  !Double -- ^ z coord
   deriving (Show, Eq)
 
 instance Num Vector3 where
@@ -21,15 +24,18 @@ instance Num Vector3 where
   signum :: Vector3 -> Vector3
   signum vec = if magnitudeSquared vec > 0 then 1 else 0
   fromInteger :: Integer -> Vector3
-  fromInteger n = Vector3 x x x where x = fromIntegral n
+  fromInteger n = toVec x where x = fromIntegral n
   negate :: Vector3 -> Vector3
   negate (Vector3 x y z) = Vector3 (negate x) (negate y) (negate z)
 
 instance Fractional Vector3 where
   fromRational :: Rational -> Vector3
-  fromRational ra = Vector3 x x x where x = fromRational ra
+  fromRational ra = toVec x where x = fromRational ra
   (/) :: Vector3 -> Vector3 -> Vector3
   (Vector3 x y z) / (Vector3 x' y' z') = Vector3 (x/x') (y/y') (z/z')
+
+toVec :: Double -> Vector3
+toVec n = Vector3 n n n
 
 magnitudeSquared :: Vector3 -> Double
 magnitudeSquared (Vector3 x y z) = x*x + y*y + z*z
@@ -40,3 +46,10 @@ magnitude = sqrt . magnitudeSquared
 infixl 7 `dot`
 dot :: Vector3 -> Vector3 -> Double
 dot (Vector3 x y z) (Vector3 x' y' z') = x*x' + y*y' + z*z'
+
+normalize :: Vector3 -> Vector3
+normalize vec = vec * toVec (1/magnitude vec)
+
+-- | Project the first vector onto the second one.
+project :: Vector3 -> Vector3 -> Vector3
+project vec line = vec * toVec ((vec `dot` line) / (vec `dot` vec))
