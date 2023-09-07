3c703b23adaace77aa3f0cacbd656cb4a9c8dd60
kintrix007 <boldizsar02@gmail.com>
2023-09-07 15:44:42 +0200

    Fully implement triangle-line intersection

--- src/Tri.hs
+++ src/Tri.hs
@@ -1,6 +1,10 @@
 module Tri
-  (Tri (Tri)
-  , isOnTri) where
+  (Tri (..)
+  , isOnTri
+  , Plane (..)
+  , Line (..)
+  , planeOf
+  , triLineIntersects) where
 
 import           Vector3
 
@@ -9,6 +13,51 @@ data Tri = Tri
   !Vector3 -- ^ Vertex b
   !Vector3 -- ^ Vertex c
   deriving (Show, Eq)
+
+data Plane = Plane
+  !Vector3 -- ^ Normal of the plane
+  !Vector3 -- ^ A point on the plane
+  deriving (Show, Eq)
+
+data Line = Line
+  !Vector3 -- ^ Direction of the line
+  !Vector3 -- ^ A point on the line
+  deriving (Show, Eq)
+
+data LineIntersectionResult
+  = NoIntersection
+  | LineContained
+  | IntersectionPoint !Vector3
+  deriving (Show)
+
+triLineIntersects :: Tri -> Line -> Bool
+triLineIntersects tri line =
+  case intersect of
+    NoIntersection      -> False
+    LineContained       -> True
+    IntersectionPoint p -> isOnTri p tri
+  where
+    intersect = planeLineIntersection (planeOf tri) line
+
+planeOf :: Tri -> Plane
+planeOf (Tri a b c) = Plane normal a
+  where
+    vab = b - a
+    vac = c - a
+    normal = vab `cross` vac
+
+planeLineIntersection :: Plane -> Line -> LineIntersectionResult
+planeLineIntersection (Plane pn pp) (Line lv lp) =
+  if isParallel
+    then if isLineContained
+      then LineContained
+      else NoIntersection
+    else IntersectionPoint intersectionPoint
+  where
+    isParallel = lv `dot` pn == 0
+    isLineContained = (pp - lp) `dot` pn == 0
+    intersectionPoint = lp + lv*toVec d
+    d = ((pp - lp) `dot` pn) / (lv `dot` pn)
 
 isOnTri :: Vector3 -> Tri -> Bool
 isOnTri p (Tri a b c) =
